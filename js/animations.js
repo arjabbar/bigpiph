515cb1bffefc55e445674a51ac9c20acf23fcfec
Abdur Jabbar <arjabbar@yahoo.com>
2014-01-29 17:14:23 -0600

    Reahed a stopping point.

--- js/animations.js
+++ js/animations.js
@@ -7,4 +7,20 @@ $(document).ready(function(e){
       $(elem).addClass( quotesAnimation + ' animated' );
     }, index * 250);
   });
+
+  var fillWithSVG = function(element){
+    $elem = $(element);
+    $elem.css('position', 'relative');
+    snap = new Snap($elem.width(), $elem.height());
+    snap.attr({position: 'absolute', top: 0, left: 0});
+    $elem.append(snap.node);
+    return snap;
+  };
+
+  $('a.red').each(function(index, elem){
+    snap = fillWithSVG(elem);
+    // rect = snap.rect(0, 0, '100%', '100%');
+  });
+
+
 });
